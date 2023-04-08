ba26e37bef636a5a1f17a1d55568636af21b5456
klisangn <kl.lisa.nguyen@gmail.com>
2023-04-08 18:16:59 +0200

    add unit test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,5 +1,8 @@
 import json
 import unittest
+import os
+import pandas as pd
+import requests
 
 from api import app
 
@@ -13,5 +16,18 @@ class TestApp(unittest.TestCase):
         response = self.app.get("/")
         self.assertEqual(response.data.decode(), "Hello World!")
 
+    def test_pred(self):
+        parent_dir = os.path.abspath('..')
+        file_path = os.path.join(parent_dir, "data", "sample.csv")
+        data = pd.read_csv(file_path)
+        x = data[data['SK_ID_CURR'] == 100028]
+        url = 'http://localhost:5000/predict'
+        params = x.to_json()
+        r = requests.post(url, json=params)
+        pred = r.json()
+        opt_pred = 1 if pred >= opt_threshold else 0
+        self.assertEqual(opt_pred, 1)
+
+        
 if __name__ == '__main__':
     unittest.main()
